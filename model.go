0ff014012aa7d0c70109e21afb9a658d87fd1e5a
逍遥子（刘星宇） <liuxingyu@soyoung.com>
2021-09-22 22:30:11 +0800

    fixed model

--- model.go
+++ model.go
@@ -12,9 +12,8 @@ import (
 type TableMeta struct {
 	tableName string
 	columns   []*ColumnMeta
-	// key 是字段名
-	fieldMap map[string]*ColumnMeta
-	typ      reflect.Type
+	fieldMap  map[string]*ColumnMeta
+	typ       reflect.Type
 }
 
 // ColumnMeta represents model's field, or column
@@ -29,64 +28,51 @@ type ColumnMeta struct {
 type tableMetaOption func(meta *TableMeta)
 
 type MetaRegistry interface {
-	// 这里传过来的 table 应该是结构体指针，例如 *User
 	Get(table interface{}, opts ...tableMetaOption) (*TableMeta, error)
 	Register(table interface{}, opts ...tableMetaOption) (*TableMeta, error)
 }
 
 var defaultMetaRegistry = &tagMetaRegistry{}
 
-// 我们的默认实现，它使用如下的规则
-// 1. 结构体名字转表名，按照驼峰转下划线的方式
-// 2. 字段名转列名，也是按照驼峰转下划线的方式
 type tagMetaRegistry struct {
-	metas      sync.Map
-	metasMutex sync.Mutex
+	metas sync.Map
 }
 
 func (t *tagMetaRegistry) Get(table interface{}) (*TableMeta, error) {
-	// 从 metas 里面去取，没有的话就调用 Register 新注册一个
+
 	if v, ok := t.metas.Load(reflect.TypeOf(table)); ok {
 		return v.(*TableMeta), nil
 	}
-	tableMeta, err := t.Register(table)
-	return tableMeta, err
+	return t.Register(table)
 }
 
 func (t *tagMetaRegistry) Register(table interface{}, opts ...tableMetaOption) (*TableMeta, error) {
-	// 拿到 table 的反射
-	// 遍历 table 的所有字段
-	// 对每一个字段检查 tag eql, 如果 eql 里面有 auto_increment, primary_key，就相应设置 ColumnMeta的值
-	// 最后处理所有的 opts
-	// 塞到 map 里面
-	t.metasMutex.Lock()
-	defer t.metasMutex.Unlock()
-	columnMeta := &ColumnMeta{}
-	v := reflect.ValueOf(table).Elem()
+	rtype := reflect.TypeOf(table)
+	v := rtype.Elem()
+	columnMeta := []*ColumnMeta{}
+	fieldMap := make(map[string]*ColumnMeta)
 	for i := 0; i < v.NumField(); i++ {
-		structField := v.Type().Field(i)
+		dataMeta := &ColumnMeta{}
+		structField := v.Field(i)
 		tag := structField.Tag.Get("eql")
 		isAuto := strings.Contains(tag, "auto_increment")
 		isKey := strings.Contains(tag, "primary_key")
-		columnMeta.columnName = structField.Name
-		columnMeta.fieldName = internal.UnderscoreName(structField.Name)
-		columnMeta.typ = structField.Type
-		if isAuto {
-			columnMeta.isAutoIncrement = true
-		}
-		if isKey {
-			columnMeta.isPrimaryKey = true
-		}
+		dataMeta.columnName = internal.UnderscoreName(structField.Name)
+		dataMeta.fieldName = structField.Name
+		dataMeta.typ = structField.Type
+		dataMeta.isAutoIncrement = isAuto
+		dataMeta.isPrimaryKey = isKey
+		columnMeta = append(columnMeta, dataMeta)
+		fieldMap[dataMeta.fieldName] = dataMeta
 	}
 	TableMeta := &TableMeta{}
+	TableMeta.columns = columnMeta
+	TableMeta.tableName = internal.UnderscoreName(v.Name())
+	TableMeta.typ = rtype
+	TableMeta.fieldMap = fieldMap
 	for _, o := range opts {
 		o(TableMeta)
 	}
-	TableMeta.columns = append(TableMeta.columns, columnMeta)
-	TableMeta.tableName = internal.TableName(table)
-	TableMeta.typ = reflect.TypeOf(table)
-	TableMeta.fieldMap = make(map[string]*ColumnMeta, 0)
-	TableMeta.fieldMap[columnMeta.fieldName] = columnMeta
 	t.metas.Store(reflect.TypeOf(table), TableMeta)
 	return TableMeta, nil
 
