0d666694a7b113afb20a496aa20886f51a115278
逍遥子（刘星宇） <liuxingyu@soyoung.com>
2021-09-22 18:41:02 +0800

    add model

--- model.go
+++ model.go
@@ -1,66 +1,94 @@
-// Copyright 2021 gotomicro
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package eql
 
 import (
 	"reflect"
+	"strings"
 	"sync"
+
+	"github.com/gotomicro/eql/internal"
 )
 
 // TableMeta represents data model, or a table
 type TableMeta struct {
 	tableName string
-	columns   []ColumnMeta
+	columns   []*ColumnMeta
+	// key 是字段名
+	fieldMap map[string]*ColumnMeta
+	typ      reflect.Type
 }
 
 // ColumnMeta represents model's field, or column
 type ColumnMeta struct {
 	columnName      string
+	fieldName       string
+	typ             reflect.Type
 	isPrimaryKey    bool
 	isAutoIncrement bool
-	notNull         bool
-	re              reflect.Type
 }
 
-type MetaTableFactory interface {
-	Get(tableName interface{}) (*ColumnMeta, error)
-	Set(tableName interface{}, column *ColumnMeta) (*ColumnMeta, error)
+type tableMetaOption func(meta *TableMeta)
+
+type MetaRegistry interface {
+	// 这里传过来的 table 应该是结构体指针，例如 *User
+	Get(table interface{}, opts ...tableMetaOption) (*TableMeta, error)
+	Register(table interface{}, opts ...tableMetaOption) (*TableMeta, error)
 }
 
-type NewMetaTable struct {
-	syMap   sync.Map
-	syMutex sync.Mutex
+var defaultMetaRegistry = &tagMetaRegistry{}
+
+// 我们的默认实现，它使用如下的规则
+// 1. 结构体名字转表名，按照驼峰转下划线的方式
+// 2. 字段名转列名，也是按照驼峰转下划线的方式
+type tagMetaRegistry struct {
+	metas      sync.Map
+	metasMutex sync.Mutex
 }
 
-func (meta *NewMetaTable) Get(tableName interface{}) (*ColumnMeta, error) {
-	if v, ok := meta.syMap.Load(tableName); ok {
-		return v.(*ColumnMeta), nil
+func (t *tagMetaRegistry) Get(table interface{}) (*TableMeta, error) {
+	// 从 metas 里面去取，没有的话就调用 Register 新注册一个
+	if v, ok := t.metas.Load(reflect.TypeOf(table)); ok {
+		return v.(*TableMeta), nil
 	}
-	return nil, nil
+	t.metasMutex.Lock()
+	defer t.metasMutex.Unlock()
+	tableMeta, err := t.Register(table, func(meta *TableMeta) {
+		meta.tableName = internal.TableName(table)
+		meta.typ = reflect.TypeOf(table)
+	})
+	return tableMeta, err
 }
 
-func (meta *NewMetaTable) Set(tableName interface{}, column *ColumnMeta) (*ColumnMeta, error) {
-	metaData, err := meta.Get(tableName)
-	if err != nil {
-		return nil, err
+func (t *tagMetaRegistry) Register(table interface{}, opts ...tableMetaOption) (*TableMeta, error) {
+	// 拿到 table 的反射
+	// 遍历 table 的所有字段
+	// 对每一个字段检查 tag eql, 如果 eql 里面有 auto_increment, primary_key，就相应设置 ColumnMeta的值
+	// 最后处理所有的 opts
+	// 塞到 map 里面
+	ColumnMeta := &ColumnMeta{}
+	v := reflect.ValueOf(table).Elem()
+	for i := 0; i < v.NumField(); i++ {
+		structField := v.Type().Field(i)
+		tag := structField.Tag.Get("eql")
+		isAuto := strings.Contains(tag, "auto_increment")
+		isKey := strings.Contains(tag, "primary_key")
+		ColumnMeta.columnName = structField.Name
+		ColumnMeta.fieldName = internal.UnderscoreName(structField.Name)
+		ColumnMeta.typ = structField.Type
+		if isAuto {
+			ColumnMeta.isAutoIncrement = true
+		}
+		if isKey {
+			ColumnMeta.isPrimaryKey = true
+		}
 	}
-	if metaData != nil {
-		return metaData, nil
+	TableMeta := &TableMeta{}
+	for _, o := range opts {
+		o(TableMeta)
 	}
-	meta.syMutex.Lock()
-	defer meta.syMutex.Unlock()
-	meta.syMap.Store(tableName, column)
-	return column, nil
+	TableMeta.columns = append(TableMeta.columns, ColumnMeta)
+	TableMeta.tableName = internal.TableName(table)
+	TableMeta.typ = reflect.TypeOf(table)
+	TableMeta.fieldMap[ColumnMeta.fieldName] = ColumnMeta
+	return TableMeta, nil
+
 }
