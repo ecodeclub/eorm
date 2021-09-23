779ad205387917b630802d494f79272261688d2c
逍遥子（刘星宇） <liuxingyu@soyoung.com>
2021-09-23 16:06:50 +0800

    tagMetaRegistry: default implementation of MetaRegistry

---
+++ internal/common_fun.go
@@ -0,0 +1,35 @@
+// Copyright 2021 gotomicro
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package internal
+
+import (
+	"unicode"
+)
+
+// UnderscoreName function mainly converts upper case to lower case and adds an underscore in between
+func UnderscoreName(tableName string) string {
+	buf := []byte{}
+	for i, v := range tableName {
+		if unicode.IsUpper(v) {
+			if i != 0 {
+				buf = append(buf, '_')
+			}
+			buf = append(buf, byte(unicode.ToLower(v)))
+		} else {
+			buf = append(buf, byte(v))
+		}
+
+	}
+	return string(buf)
+}
