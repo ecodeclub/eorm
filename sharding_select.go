babaf7aad7792e7cda05a5b6be9579088b008cc4
Stone-afk <1711865140@qq.com>
2023-02-06 12:56:49 +0800

    feature add fidDst

--- sharding_select.go
+++ sharding_select.go
@@ -17,6 +17,7 @@ package eorm
 import (
 	"context"
 	"errors"
+	"fmt"
 )
 
 type ShardingSelector[T any] struct {
@@ -24,6 +25,7 @@ type ShardingSelector[T any] struct {
 	table   *T
 	db      *ShardingDB
 	where   []Predicate
+	having  []Predicate
 	columns []Selectable
 
 	orderBy []string
@@ -46,7 +48,86 @@ func (s *ShardingSelector[T]) build(db, tbl string) (*ShardingQuery, error) {
 
 func (s *ShardingSelector[T]) findDsts() ([]Dst, error) {
 	//  通过遍历 pre 查找目标 shardingkey
+	if len(s.where) > 0 {
+		pre := s.where[0]
+		for i := 1; i < len(s.where)-1; i++ {
+			pre = pre.And(s.where[i])
+		}
+		return s.findDstByPredicate(pre)
+	}
+	if len(s.having) > 0 {
+
+	}
 	panic("implement me")
+}
+
+func (s *ShardingSelector[T]) findDstByPredicate(pre Predicate) ([]Dst, error) {
+	var res []Dst
+	switch pre.op {
+	case opAnd, opOr:
+		left, err := s.findDstByPredicate(pre.left.(Predicate))
+		if err != nil {
+			return nil, err
+		}
+		if len(left) == 0 {
+			return s.findDstByPredicate(pre.right.(Predicate))
+		}
+		right, err := s.findDstByPredicate(pre.right.(Predicate))
+		if err != nil {
+			return nil, err
+		}
+		if len(right) == 0 {
+			return left, nil
+		}
+		return s.merge(left, right), nil
+	case opGT:
+
+	case opLT:
+
+	case opEQ:
+		DBSf := s.meta.DBShardingFunc
+		TbSf := s.meta.TableShardingFunc
+		col, isCol := pre.left.(Column)
+		right, isVals := pre.right.(values)
+		if !isCol || !isVals {
+			return nil, errors.New("too complex query, temporarily not supported")
+		}
+		skVal := right.data[0]
+		if col.name == s.meta.ShardingKey {
+			shardingDB, err := DBSf(skVal)
+			if err != nil {
+				return nil, errors.New("execute sharding algorithm err")
+			}
+			shardingTbl, err := TbSf(skVal)
+			if err != nil {
+				return nil, errors.New("execute sharding algorithm err")
+			}
+			dst := Dst{
+				DB:    shardingDB,
+				Table: shardingTbl,
+			}
+			res = append(res, dst)
+		}
+	default:
+		return nil, fmt.Errorf("eorm: operators that do not know how to handle %v", pre.op.text)
+	}
+	return res, nil
+}
+
+func (s *ShardingSelector[T]) merge(left, right []Dst) []Dst {
+	res := make([]Dst, 0, len(left)+len(right))
+	for _, r := range right {
+		exist := false
+		for _, l := range left {
+			if r.DB == l.DB && r.Table == l.Table {
+				exist = true
+			}
+		}
+		if exist {
+			res = append(res, r)
+		}
+	}
+	return res
 }
 
 func (s *ShardingSelector[T]) Get(ctx context.Context) (*T, error) {
