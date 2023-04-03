2fdff121f87f97d33ec0c0a6e52e9438cba40544
hookokoko <hookokoko@126.com>
2023-04-03 21:08:35 +0800

    修改defer格式

--- core.go
+++ core.go
@@ -33,7 +33,11 @@ type core struct {
 
 func getHandler[T any](ctx context.Context, sess Session, c core, qc *QueryContext) *QueryResult {
 	rows, err := sess.queryContext(ctx, qc.q.SQL, qc.q.Args...)
-	defer rows.Close()
+	defer func() {
+		if rows != nil {
+			_ = rows.Close()
+		}
+	}()
 	if err != nil {
 		return &QueryResult{Err: err}
 	}
@@ -70,6 +74,11 @@ func get[T any](ctx context.Context, sess Session, core core, qc *QueryContext) 
 
 func getMultiHandler[T any](ctx context.Context, sess Session, c core, qc *QueryContext) *QueryResult {
 	rows, err := sess.queryContext(ctx, qc.q.SQL, qc.q.Args...)
+	defer func() {
+		if rows != nil {
+			_ = rows.Close()
+		}
+	}()
 	if err != nil {
 		return &QueryResult{Err: err}
 	}
